54f9f5aa0beb70814ec623f81f7682e44c4cadb4
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2024-02-14 20:03:40 +0530

    Removed redundancies, made output more verbose

--- Day-3/3-day3-TOH.c
+++ Day-3/3-day3-TOH.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define UNDERFLOW(s) ((s->top)==(-1)?(1):(0))
-#define OVERFLOW(s) ((s->top)==((s->MAX)-1)?(1):(0))
-
 /*
 underlying structure
 UNDERFLOW and OVERFLOW have been neglected since the solution to TOH has been designed such that those cases do not occur
 */
+
 typedef struct stack{
 	int MAX;
 	int top;
 	int* items;
-	char* name;
+	char* name; //to store the name of the stack, which gets printed
 } stack;
 
 stack newStack(int length, char* name){
@@ -36,28 +34,23 @@ void push(stack* s, int item){
 }
 
 int pop(stack* s){
-	int item;
-	item = s->items[(s->top)--];
-	return item;
+	return s->items[(s->top)--];
 }
 /*End of underlying structure*/
-
 
 //Tower of Hanoi - Recursive solution using stacks
 void TOH(int n, stack* src, stack* dest, stack* aux, int* moves){
 	int disc;
 	if(n==1){
 		disc = pop(src);
-		printf("Moved disc %d from %s to %s\n", n, src->name, dest->name);
+		printf("%d. Moved disc %d from %s to %s\n", ++(*moves), n, src->name, dest->name);
 		push(dest, disc);
-		(*moves)++;
 	}
 	else{
 		TOH(n-1, src, aux, dest, moves);
 		disc = pop(src);
-		printf("Moved disc %d from %s to %s\n", n, src->name, dest->name);
+		printf("%d. Moved disc %d from %s to %s\n", ++(*moves), n, src->name, dest->name);
 		push(dest, disc);
-		(*moves)++;
 		TOH(n-1, aux, dest, src, moves);
 	}
 }
@@ -72,9 +65,9 @@ int main(){
 		printf("Number of discs cannot be zero\n");
 		exit(1);
 	}
-	src = newStack(N_DISCS, SRC);
-	dest = newStack(N_DISCS, DEST);
-	aux = newStack(N_DISCS, AUX);
+	src = newStack(N_DISCS, "src");
+	dest = newStack(N_DISCS, "dest");
+	aux = newStack(N_DISCS, "aux");
 	for(i=N_DISCS; i>0; i--) push(&src, i); //initialize source
 	printf("\nInitial state:\n");                                                                    
 	display(&src);                                             
